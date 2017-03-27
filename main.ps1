dac2bad1f0ebb4d96b64554e5b419e9e376c8195
dan725 <dyang@ucsd.edu>
2017-03-27 04:07:57 -0700

    Added functionality to handle old/existing users.
    * Added loop, so it prompts user until correct input.
    * User can now disable science users not in current mission ID.  It
    moves the disabled users to OU=Disabled Users.
    * Existing science users from OU=Disabled Users that is recognized from
    the .csv file is automatically enabled and moved back to OU=Science,
    with description changed to current mission ID + SCIENCE.
    * Cleaned up the script, and added more comments
    * Removed some comments and code that was only relevant to the original
    script, which only added Sally Ride science users from a file from a
    specific location.

--- main.ps1
+++ main.ps1
@@ -18,138 +18,167 @@ $dnsroot = '@' + (Get-ADDomain).dnsroot
 #OU=STS,OU=Users,OU=SOMTS,OU=SIO,DC=AD,DC=UCSD,DC=EDU
 #CN=someuser,OU=People,DC=AD,DC=UCSD,DC=EDU
 
-#Import user CSV with user info.
-#$users = Import-CSV C:\users\sts-cr\desktop\users.csv 
-
-#Prompt user if they want to add users, or remove old users only.  Remove old users to be added in the future.
-$delonly = Read-Host -Prompt '[0]Do you want to Add Users (removes old users as well)[0], or [1]Remove old users only[1]? 
-Type number 0, or 1, then ENTER.  Do not press 1 yet.  Option not yet available.'
-
-
-try
+#Do/While proper input, User prompt if user wants to add or remove users.
+Do
 {
-    if($delonly -eq 0) 
-    {
-        $missionID = Read-Host -Prompt 'Input mission ID (ex: SR1705)'
-        $missionID = $missionID.ToUpper()
-        #prompt for password, hide password. 
-        $defpassword = Read-Host -AsSecureString 'Enter the default password (usually same as wifi)'
+    $delonly = Read-Host -Prompt '[0]Do you want to Add Users[0], or [1]Remove old users[1]?
+    Type number 0, or 1, then ENTER.'
 
-    #Prompt to choose .CSV file, then import user .CSV.
-    try 
+
+    #################### Try, then catch all system exceptions ####################
+    try
     {
-        Write-Host "Choose the proper .csv file."
-        $inputfile = Get-FileName 
-        $users = Import-CSV $inputfile
+        #################### If Users are being added ####################
+        if($delonly -eq 0) 
+        {
+            #Prompt for mission ID
+            $missionID = Read-Host -Prompt 'Input current mission ID (ex: SR1705).'
+            $missionID = $missionID.ToUpper()
+            #prompt for password, hide password. 
+            $defpassword = Read-Host -AsSecureString 'Enter the default password (usually same as wifi)'
+            #Prompt to choose .CSV file, then import user .CSV.
+            try 
+            {
+                Write-Host "Choose the proper .csv file."
+                $inputfile = Get-FileName 
+                $users = Import-CSV $inputfile
+            }
+            catch [System.Object]
+            {
+                Write-Output "User cancelled, $_"
+            }
+
+            #Grab all users in OU=Disabled Users, and test during loop.
+            $ExistingUsers = Get-ADUser -Filter * -SearchBase 'OU=Disabled Users,CN=Users,DC=rv-sallyride,DC=ucsd,DC=edu' 
+
+            ##### Loop through $users memory, test if user exists, ####
+            ##### else sort them in science or crew                ####
+            foreach ($user in $users) 
+            {
+                try
+                {
+                    #Add users if description in csv file is science
+                    if($user.Description -eq "science")
+                    {                        
+                        try 
+                        {
+                            #Loop to test each new user against disabled users, and add and enable them to current mission
+                            foreach ($euser in $ExistingUsers)
+                            {
+                                if($euser.SamAccountName -eq $user.SamAccountName -and $euser.GivenName -eq $user.Firstname)
+                                {
+                                     Set-ADUser $euser -Description ($missionID+ " " + $user.Description.ToUpper())
+                                     Get-ADUser $euser| Move-ADObject -TargetPath `
+                                     'OU=Science,CN=Users,DC=rv-sallyride,DC=ucsd,DC=edu' 
+                                     Write-Output "Moved existing user $($euser.SamAccountName) to Science"
+                                     Enable-ADAccount -Identity $euser.SamAccountName
+                                     Write-Output "Disabled user $($euser.SamAccountName) now re-enabled" 
+                                     $test = @{$user = 1}  #Add $user property to object $test, so catch error is suppressed
+
+                                }
+                            }
+
+                            New-ADUser -Name ($user.Firstname + " " + $user.LastName) -SamAccountName ($user.SamAccountName) `
+                            -Description ($missionID + " " + $user.Description.ToUpper()) `
+                            -DisplayName ($user.Firstname + " " + $user.LastName) -GivenName ($user.FirstName) -Surname ($user.LastName) `
+                            -UserPrincipalName ($user.SamAccountName + $dnsroot) `
+                            -AccountPassword $defpassword -PassThru `
+                            -ChangePasswordAtLogon $true `
+                            -Enabled $true `
+                            -Path "OU=Science,CN=Users,DC=rv-sallyride,DC=ucsd,DC=edu"
+                            Add-ADGroupMember -Identity www-science -Members $user.SamAccountName 
+                         }
+                         catch [System.Object]
+                         {
+                                if($test.$user -eq 1)  #If $test object's property $user exists, supress error
+                                {}
+                                else
+                                { 
+                                    Write-Output "Could not group user $($user.SamAccountName), $_ " 
+                                }
+                         }        
+                    }
+                    #Add users if description field in csv file is crew                       
+                    Elseif($user.Description -eq "crew") 
+                    {         
+                        try 
+                        {
+                            New-ADUser -Name ($user.Firstname + " " + $user.LastName) -SamAccountName ($user.SamAccountName) `
+                            -Description ($user.Description.ToUpper()) `
+                            -DisplayName ($user.Firstname + " " + $user.LastName) -GivenName ($user.FirstName) -Surname ($user.LastName) `
+                            -UserPrincipalName ($user.SamAccountName + $dnsroot) `
+                            -AccountPassword $defpassword -PassThru `
+                            -ChangePasswordAtLogon $true `
+                            -Enabled $true `
+                            -Path "OU=SR Crew,CN=Users,DC=rv-sallyride,DC=ucsd,DC=edu"
+                            Add-ADGroupMember -Identity www-crew -Members $user.SamAccountName 
+                         }
+                         catch [System.Object]
+                         {
+                            Write-Output "Could not group user $($user.SamAccountName), $_ " 
+                         }    
+                     }
+                    Else
+                    {
+                        Write-Output "Please specify 'Science' or 'Crew' in the description field in `
+                        $($user.SamAccountName)'s line in the users.csv file."
+                    } 
+                 }
+                 catch [System.Object]
+                 {
+                    Write-Output "Could not create user $($user.SamAccountName), $_ " `
+                 }
+        
+            }
+        }
+
+        ################### If Users are being removed ###################
+        elseif($delonly -eq 1)
+        {
+                #Prompt for mission ID
+                $missionID = Read-Host -Prompt 'Input current mission ID (ex: SR1705). Any user not in current mission will be disabled'
+                $missionID = $missionID.ToUpper()
+                #Grab all users in OU=Science, and make sure to include Description in memory.
+                $OldUsers = Get-ADUser -Filter * -SearchBase 'OU=Science,CN=Users,DC=rv-sallyride,DC=ucsd,DC=edu' `
+                -Properties Description
+                $nouser = 0
+      
+                Try
+                {
+                    foreach ($ouser in $OldUsers) 
+                    {
+                        #Pull just the Description property of each ouser in OldUser, compare if it !includes current missionID.
+                        if($ouser.Description -notmatch "$missionID")
+                        {
+                            Set-ADUser $ouser -Description "SCIENCE"
+                            Get-ADUser $ouser| Move-ADObject -TargetPath `
+                            'OU=Disabled Users,CN=Users,DC=rv-sallyride,DC=ucsd,DC=edu' 
+                            Write-Output "Moved $($ouser.Name) to Disabled Users"
+                            Disable-ADAccount -Identity $ouser.SamAccountName
+                            Write-Output "$($ouser.SamAccountName) Disabled"
+                            $nouser++ 
+                        } 
+                    }
+                    if($nouser -eq 0)
+                    {
+                        Write-Output "No old users found"
+                    }
+                }
+                catch [System.Object]
+                {
+                    Write-Output "Exception. $_ Closing"
+                }
+          }
+        ################# Improper user input,not 1 or 0 #################
+        else
+        {
+            Write-Output "Invalid entry, Enter 1 or 0."
+        } 
     }
 
+    ######################### Catch all system exceptions #########################
     catch [System.Object]
     {
-        Write-Output "User cancelled, $_"
+            Write-Output "Exception. $_ , Closing" 
     }
-
-    foreach ($user in $users) 
-                                                                                                                                                                                                    {
-        try 
-        {
-             #There are 2 types of users: Science and Crew, Case sensitive
-             if($user.Description -eq "Science")
-                {                        
-                    try 
-                    {
-                        New-ADUser -Name ($user.Firstname + " " + $user.LastName) -SamAccountName ($user.SamAccountName) -Description ($missionID + " " + $user.Description.ToUpper()) `
-                        -DisplayName ($user.Firstname + " " + $user.LastName) -GivenName ($user.FirstName) -Surname ($user.LastName) `
-                        -UserPrincipalName ($user.SamAccountName + $dnsroot) `
-                        -AccountPassword $defpassword -PassThru `
-                        -ChangePasswordAtLogon $true `
-                        -Enabled $true `
-                        -Path "OU=Science,CN=Users,DC=rv-sallyride,DC=ucsd,DC=edu"
-                        Add-ADGroupMember -Identity www-science -Members $user.SamAccountName `
-                     }
-                     catch [System.Object]
-                     {
-                        Write-Output "Could not group user $($user.SamAccountName), $_ " `
-                     }        
-                 }
-                                    
-             Elseif($user.Description -eq "Crew") 
-                 {         
-                    try 
-                    {
-                        New-ADUser -Name ($user.Firstname + " " + $user.LastName) -SamAccountName ($user.SamAccountName) -Description ($user.Description.ToUpper()) `
-                        -DisplayName ($user.Firstname + " " + $user.LastName) -GivenName ($user.FirstName) -Surname ($user.LastName) `
-                        -UserPrincipalName ($user.SamAccountName + $dnsroot) `
-                        -AccountPassword $defpassword -PassThru `
-                        -ChangePasswordAtLogon $true `
-                        -Enabled $true `
-                        -Path "OU=SR Crew,CN=Users,DC=rv-sallyride,DC=ucsd,DC=edu"
-                        Add-ADGroupMember -Identity www-crew -Members $user.SamAccountName `
-                     }
-                     catch [System.Object]
-                     {
-                        Write-Output "Could not group user $($user.SamAccountName), $_ " `
-                     }    
-                  }
-               Else
-                  {
-                    Write-Output "Please specify 'Science' or 'Crew' (case-sensitive) in description in $($user.SamAccountName)'s line in the users.csv file."
-                  }
-                
-             }
-           
-
-        catch [System.Object]
-                {
-                Write-Output "Could not create user $($user.SamAccountName), $_ " `
-                }
-    }
-
-    #Move old mission users to Disabled Users.  To be added in future. 
- <#
-    $OldUsers = Get-ADGroupMember -Identity www-science| Import-Csv -path .\temp.csv 
-
-    ForEach ($ouser in $OldUsers) 
-     {
-      $Usercheck = Get-ADUser -Properties Description
-         if($Usercheck -notcontains $missionID) 
-         {
-             Write-Host "Moving $user to Disabled Users"
-             Get-ADUser $ouser| Move-ADObject -TargetPath 'OU=Disabled Users,CN=Users,DC=rv-sallyride,DC=ucsd,DC=edu' 
-             Set-ADUser $ouser -Description "SCIENCE"
-         }
-      }
-  #>
- 
-    }
-    elseif($delonly -eq 1)
-    {
-
-        Write-Output "Not available yet.  Closing"
-        #Move old mission users to Disabled Users.
-        <#
-        $OldUsers = Get-ADGroupMember -Identity www-science| Import-Csv -path .\temp.csv 
-
-        ForEach ($ouser in $OldUsers) 
-         {
-          $Usercheck = Get-ADUser -Properties Description
-             if($Usercheck -notcontains $missionID) 
-             {
-                 Write-Host "Moving $user to Disabled Users"
-                 Get-ADUser $ouser| Move-ADObject -TargetPath 'OU=Disabled Users,CN=Users,DC=rv-sallyride,DC=ucsd,DC=edu' 
-                 Set-ADUser $ouser -Description "SCIENCE"
-             }
-          }
-          #>
-    }
-    else
-    {
-        Write-Output "Invalid entry, Closing"
-    }
-
-}
-
-#Catch all handling.
-catch [System.Object]
-{
-        Write-Output "Exception. $_ , Closing" `
-}
+}while($delonly -ne 1 -and $delonly -ne 0)
